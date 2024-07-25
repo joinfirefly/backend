cdcac5594eb1a317290b723654846452437a4cfe
AmaseCocoa <amase.cocoa@gmail.com>
2024-07-25 14:53:58 +0900

    Fix errors due to formatting of imports and backend rewrite

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,20 +1,24 @@
-import pytest
-from unittest.mock import patch
-from main import get, generate_schema
 import logging
+from unittest.mock import patch
+
+import pytest
+
+from app.server import generate_schema
+from app.utils.log import getLogger
+from app import __version__
 
 @pytest.fixture
 def logger():
-    return get()
+    return getLogger()
 
 @pytest.mark.parametrize("level", [logging.DEBUG, logging.WARNING])
 def test_get_with_different_log_level(level, logger):
-    logger_modified = get(level)
+    logger_modified = getLogger(level=level)
     assert logger_modified.level == level
 
 def test_get_with_existing_handlers(logger):
     logger_old = logger
-    logger_new = get()
+    logger_new = getLogger()
     assert logger_old == logger_new
 
 @patch('app.server.get_openapi')
@@ -30,7 +34,7 @@ def test_generate_schema(mock_app, mock_logger, mock_get_openapi):
     
     result = generate_schema()
 
-    mock_logger.info.assert_any_call("v0.1.0, Created By Holo Team")
+    mock_logger.info.assert_any_call(f"v{__version__}, Created By Holo Team")
     mock_logger.info.assert_any_call("Generating OpenAPI JSON...")
     mock_get_openapi.assert_called_once_with(title='title', version='version', openapi_version='openapi_version', description='description', routes='routes')
     mock_logger.info.assert_called_with("OpenAPI JSON data was successfully generated.")
